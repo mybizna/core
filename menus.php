f8bd748840e0b9710ab824b381f201459f91f0aa
Dedan Irungu <dedanirungu@gmail.com>
2022-09-08 16:00:06 +0300

    Update

--- menus.php
+++ menus.php
@@ -6,12 +6,12 @@ $this->add_module_info("core", [
     'title' => 'Core',
     'description' => 'Core',
     'icon' => 'fas fa-receipt',
-    'path' => '/core',
+    'path' => '/core/admin/country',
     'class_str'=> 'text-warning border-warning'
 ]);
 
-$this->add_menu("core", "country", "Country", "/country", "fas fa-cogs", 5);
-$this->add_menu("core", "currency", "Currency", "/currency", "fas fa-cogs", 5);
-$this->add_menu("core", "setting", "Setting", "/setting", "fas fa-cogs", 5);
-$this->add_menu("core", "state", "State", "/state", "fas fa-cogs", 5);
-$this->add_menu("core", "timezone", "Timezone", "/timezone", "fas fa-cogs", 5);
+$this->add_menu("core", "country", "Country", "/core/admin/country", "fas fa-cogs", 5);
+$this->add_menu("core", "currency", "Currency", "/core/admin/currency", "fas fa-cogs", 5);
+$this->add_menu("core", "setting", "Setting", "/core/admin/setting", "fas fa-cogs", 5);
+$this->add_menu("core", "state", "State", "/core/admin/state", "fas fa-cogs", 5);
+$this->add_menu("core", "timezone", "Timezone", "/core/admin/timezone", "fas fa-cogs", 5);
