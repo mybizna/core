94fb69026be010cd5f33b3778e52a7a62a29dafc
Dedan Irungu <dedanirungu@gmail.com>
2023-01-26 12:22:50 +0300

    Adding Global Notification

---
+++ views/admin/notification/form.vue
@@ -0,0 +1,63 @@
+<template>
+    <table-edit :path_param="path_param" :model="model">
+        <FormKit v-model="model.id" label="Id" id="id" type="hidden" :readonly="true" validation="required" />
+        <div class="row">
+            <div class="col-md-12">
+                <FormKit v-model="model.slug" label="Slug" id="slug" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+
+            <div class="col-md-6">
+                <FormKit v-model="model.short" label="Short" id="short" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+            <div class="col-md-6">
+                <FormKit v-model="model.enable_short" label="enable_short" id="enable_short" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+
+            <div class="col-md-6">
+                <FormKit v-model="model.medium" label="Medium" id="medium" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+            <div class="col-md-6">
+                <FormKit v-model="model.enable_medium" label="Enable Medium" id="enable_medium" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+
+            <div class="col-md-6">
+                <FormKit v-model="model.long" label="Long" id="long" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+            <div class="col-md-6">
+                <FormKit v-model="model.enable_long" label="Enable Long" id="enable_long" type="text" :readonly="true"
+                    validation="required" />
+            </div>
+        </div>
+    </table-edit>
+</template>
+
+<script>
+export default {
+    components: {
+        TableEdit: window.$func.fetchComponent("components/common/TableEdit.vue")
+    },
+    data() {
+        return {
+            id: null,
+            path_param: ["core", "notification"],
+            model: {
+                id: "",
+                slug: "",
+                short: "",
+                medium: "",
+                long: "",
+                enable_short: "",
+                enable_medium: "",
+                enable_long: "",
+                published: "",
+            },
+        };
+    }
+};
+</script>
---
+++ views/admin/notification/list.vue
@@ -0,0 +1,42 @@
+<template>
+    <table-list title="Notification" :path_param="path_param" :search_fields="search_fields" :model="model"
+        :table_fields="table_fields"></table-list>
+</template>
+
+<script>
+export default {
+    components: {
+        TableList: window.$func.fetchComponent(
+            "components/common/TableList.vue"
+        ),
+    },
+    data() {
+        return {
+            path_param: ["core", "notification"],
+            model: {
+                slug: "",
+                short: "",
+                medium: "",
+                long: "",
+                enable_short: "",
+                enable_medium: "",
+                enable_long: "",
+                published: "",
+            },
+
+            search_fields: [
+                { type: "text", name: "slug", label: "Slug", ope: "", },
+            ],
+            table_fields: [
+                { text: "Slug", prop: "slug", name: "slug", },
+                { text: "Enable Short", prop: "enable_short", name: "enable_short", is_boolean: true, },
+                { text: "Enable Medium", prop: "enable_medium", name: "enable_medium", is_boolean: true, },
+                { text: "Enable Long", prop: "enable_long", name: "enable_long", is_boolean: true, },
+
+            ],
+        };
+
+
+    },
+};
+</script>
